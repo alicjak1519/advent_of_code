4c838a8560669e8792317322066efcda4cc329d8
alicjak1519 <akrzeminska1519@gmail.com>
2020-12-02 23:15:33 +0100

    day 2: Improve tests to test both parts of the problem

--- day2_password_philosophy/test_password_philosophy.py
+++ day2_password_philosophy/test_password_philosophy.py
@@ -3,6 +3,11 @@ from assertpy import assertpy
 from .password_philosophy import PasswordPhilosophy
 
 
-def test_example_input_result():
+def test_example_input_result_part1():
     example_philosophy = PasswordPhilosophy("inputs/day2_example_input.csv")
     assertpy.assert_that(example_philosophy.count_valid_passwords()).is_equal_to(2)
+
+
+def test_example_input_result_part2():
+    example_philosophy = PasswordPhilosophy("inputs/day2_example_input.csv")
+    assertpy.assert_that(example_philosophy.count_still_valid_passwords()).is_equal_to(1)
