91280753a1109801d351f6eb207f4bf383738dc8
alicjak1519 <akrzeminska1519@gmail.com>
2020-12-02 16:33:37 +0100

    day 1: Generalize solution to n elements

--- day1_report_repair/day1_report_repair.py
+++ day1_report_repair/day1_report_repair.py
@@ -1,19 +1,28 @@
-import pandas as pd
+from functools import reduce
+from typing import List, Tuple
+from itertools import combinations
 
 
 class ReportRepair:
     def __init__(self, path: str) -> None:
         self.input = self._read_input(path)
 
-    def _read_input(self, path):
-        return pd.read_csv(path, header=None)
+    def _read_input(self, path: str) -> List[int]:
+        with open(path) as file:
+            return [int(line) for line in file.readlines()]
 
-    def find_2020_elements(self):
-        for element in self.input[0]:
-            if (2020 - element) in self.input.values:
-                return element
-        raise
+    def _find_2020_combination(self, number: int) -> Tuple[int]:
+        for combination in combinations(self.input, number):
+            if reduce(lambda x, y: x + y, combination) == 2020:
+                return combination
 
-    def find_product_result(self):
-        element = self.find_2020_elements()
-        return element * (2020 - element)
+    def multiply_2020_combination(self, number: int) -> int:
+        return reduce(lambda x, y: x * y, self._find_2020_combination(number))
+
+
+if __name__ == "__main__":
+    report_repair = ReportRepair("../inputs/day1_example_input.csv")
+    solution_for_two_elements = report_repair.multiply_2020_combination(2)
+    solution_for_three_elements = report_repair.multiply_2020_combination(3)
+    print(f"Solution for two elements: {solution_for_two_elements}")
+    print(f"Solution for three elements: {solution_for_three_elements}")
--- day1_report_repair/test_day1_report_repair.py
+++ day1_report_repair/test_day1_report_repair.py
@@ -5,9 +5,4 @@ from .day1_report_repair import ReportRepair
 
 def test_example_input_result():
     example_repair = ReportRepair("inputs/day1_example_input.csv")
-    assertpy.assert_that(example_repair.find_product_result()).is_equal_to(514579)
-
-
-def test_input_result():
-    example_repair = ReportRepair("inputs/day1_input.csv")
-    assertpy.assert_that(example_repair.find_product_result()).is_equal_to(974304)
+    assertpy.assert_that(example_repair.multiply_2020_combination(2)).is_equal_to(514579)
